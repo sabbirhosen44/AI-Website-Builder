fb17cc9250dd358a9bc0e42d33980884cf66274d
Jawwad926 <mdsabbirhosen926@gmail.com>
2026-01-07 16:31:57 +0600

    feat: implement Preview component to render project

--- frontend/src/pages/Preview.tsx
+++ frontend/src/pages/Preview.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { dummyProjects } from "@/assets/DummyData";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import PreviewPanel from "@/components/PreviewPanel";
+import type { Project } from "@/types";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Preview = () => {
-  return <div>Preview</div>;
+  const { projectId, versionId } = useParams();
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchCode = () => {
+    const foundProject = dummyProjects.find(
+      (project) => project.id === projectId
+    );
+    setTimeout(() => {
+      if (foundProject) setCode(foundProject.current_code);
+      setLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    fetchCode();
+  }, [projectId]);
+
+  if (loading) <LoadingSpinner />;
+
+  return (
+    <div className="w-full h-screen bg-gray-900 fade-in">
+      {code && (
+        <PreviewPanel
+          project={{ current_code: code } as Project}
+          isGenerating={false}
+          showEditorPanel={false}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Preview;
